cc98f00fb15baf2857af6da29aae46db7c490e67
lupo <bensender@gmail.com>
2022-09-21 12:51:43 +0300

    Reading stock data to stream

--- src/main/java/com/lupo/interview/accessFintech/config/WebClientConfig.java
+++ src/main/java/com/lupo/interview/accessFintech/config/WebClientConfig.java
@@ -11,10 +11,7 @@ public class WebClientConfig {
     @Bean
     public WebClient getWebClient() {
         WebClient client = WebClient.builder()
-                .baseUrl("https://s3.amazonaws.com")
-//                .defaultCookie("cookieKey", "cookieValue")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-//                .defaultUriVariables(Collections.singletonMap("url", "http://localhost:8080"))
                 .build();
         return client;
     }
--- src/main/java/com/lupo/interview/accessFintech/service/StockClient.java
+++ src/main/java/com/lupo/interview/accessFintech/service/StockClient.java
@@ -3,12 +3,16 @@ package com.lupo.interview.accessFintech.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyExtractors;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -19,18 +23,57 @@ public class StockClient {
     private final WebClient client;
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    public StockClient(@Autowired WebClient webClient) {
+
+    private final String stockUrl;
+
+    public StockClient(@Autowired WebClient webClient, @Value("${server.stock.provider.url}") String stockUrl) {
         this.client = webClient;
+        this.stockUrl = stockUrl;
     }
 
     @Scheduled(fixedRateString = "${server.scheduler.interval.millis}")
     public void pollData() {
 
-        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = client.method(HttpMethod.GET);
-        WebClient.RequestBodySpec requestBodySpec = uriSpec.uri("/test-data-samples/stocks.json");
-        Mono<String> response = requestBodySpec.retrieve()
-                .bodyToMono(String.class);
-        LOG.info(response.block());
+        try {
+            InputStream is = getResponseAsInputStream(client, stockUrl);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line = br.readLine()) != null) {
+                LOG.info(line);
+            }
+        } catch (IOException | InterruptedException e) {
+            LOG.error("Failed to retrieve stock from stock provider");
+        }
         LOG.info("The time is now {}", dateFormat.format(new Date()));
+    }
+    public static InputStream getResponseAsInputStream(WebClient client, String url) throws IOException, InterruptedException {
+
+        PipedOutputStream pipedOutputStream = new PipedOutputStream();
+        PipedInputStream pipedInputStream = new PipedInputStream(1024 * 10);
+        pipedInputStream.connect(pipedOutputStream);
+
+        Flux<DataBuffer> body = client.get()
+                .uri(url)
+                .exchangeToFlux(clientResponse -> {
+                    return clientResponse.body(BodyExtractors.toDataBuffers());
+                })
+                .doOnError(error -> {
+                    LOG.error("error occurred while reading body", error);
+                })
+                .doFinally(s -> {
+                    try {
+                        pipedOutputStream.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .doOnCancel(() -> {
+                    LOG.error("Get request is cancelled");
+                });
+
+        DataBufferUtils.write(body, pipedOutputStream)
+                .log("Writing to output buffer")
+                .subscribe();
+        return pipedInputStream;
     }
 }
