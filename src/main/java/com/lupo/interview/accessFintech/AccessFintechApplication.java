ce65383153385b47e625872cfe5691428cec3797
lupo <bensender@gmail.com>
2022-09-21 11:09:23 +0300

    Initial commit

---
+++ src/main/java/com/lupo/interview/accessFintech/AccessFintechApplication.java
@@ -0,0 +1,13 @@
+package com.lupo.interview.accessFintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessFintechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessFintechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lupo/interview/accessFintech/AccessFintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lupo.interview.accessFintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessFintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
